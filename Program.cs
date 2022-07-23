c8fe7cd3634e513cab497695361de5869ce2100c
Jeffry M. Rivera <jeffrymr92@gmail.com>
2022-07-23 12:47:50 -0500

    Ejercicio 1 y 2

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿
+Console.WriteLine("Ingrese su nombre: ");
+string nombre = Console.ReadLine();
+Console.WriteLine("Hola " + nombre);
+
+Console.WriteLine("La hora es: " + DateTime.Now.ToShortTimeString());
